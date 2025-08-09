9fe16d6cbc0ccc17357db484a95cf45f01a51026
Bhagwan Golhar <bhagwangolhar6629@gmail.com>
2025-08-09 10:09:36 +0530

    added a functionality to'%' this button

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ console.log(remove.innerText);
 for (let button of buttons) {
   button.addEventListener("click", function (Icon) {
     console.log("Button clicked");
-
     main_container.classList.add("animate-gradient");
 
     setTimeout(() => {
@@ -16,24 +15,53 @@ for (let button of buttons) {
     }, 150); // match animation time
 
     let clickValue = this.innerText;
+    let expr = inputField.value;
 
     if (clickValue === "C") {
       inputField.value = "";
+    } else if(clickValue === "%"){
+      handlePercentage();
     } else if (clickValue === "=") {
       try {
-        let result = eval(inputField.value);
+        let result = eval(expr.replace(/%/g, "%"));
         inputField.value = result;
       } catch (error) {
         inputField.value = "Error";
       }
     } else if(clickValue === "D"){
       inputField.value = inputField.value.slice(0,-1);
-    } else if(clickValue === "%"){
-      if(inputField.value){
-        inputField.value = parseFloat(inputField.value)/100;
-      } 
     } else {
       inputField.value += clickValue;
     }
   });
+}
+
+function handlePercentage() {
+    let expr = inputField.value;
+
+    // Try matching "number operator number%"
+    let match = expr.match(/(-?\d+(\.\d+)?)([+\-*/])(-?\d+(\.\d+)?)$/);
+
+    if (match) {
+        let firstNum = parseFloat(match[1]);
+        let operator = match[3];
+        let secondNum = parseFloat(match[4]);
+
+        let result;
+        if (operator === "+" || operator === "-") {
+            // % means percentage of first number
+            result = firstNum * (secondNum / 100);
+        } else if (operator === "*" || operator === "/") {
+            // % means fraction (0.XX)
+            result = secondNum / 100;
+        }
+
+        inputField.value = expr.replace(/(-?\d+(\.\d+)?)([+\-*/])(-?\d+(\.\d+)?)$/, 
+                                        `${firstNum}${operator}${result}`);
+    } 
+    else {
+        // Single number case → convert to fraction
+        let num = parseFloat(expr) || 0;
+        inputField.value = num / 100;
+    }
 }
